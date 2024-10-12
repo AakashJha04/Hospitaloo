b54789bb5b818e372bcc6ab3299d16377643da20
AakashJha04 <aakashthedeveloper04@gmail.com>
2024-10-12 18:30:52 +0530

    changes in  About us page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -11,16 +11,18 @@ const About = () => {
       <div className='my-10 flex flex-col md:flex-row gap-12 '>
         <img className='w-full md:max-w-[360px]' src={about_image} alt=''/>
         <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
-          <p>Welcome to Hospitallo the portal to book Best Doctors</p>
+          <h3>Welcome to Hospitallo, where compassion meets excellence in healthcare. We are dedicated to providing comprehensive medical services with a patient-first approach, ensuring that every individual receives personalized care in a comfortable and supportive environment.</h3>
+          
+          <b className='text-gray-800'>Our Mission</b>
           <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          At Hospitallo, our mission is to deliver high-quality healthcare that focuses on improving the well-being of our community. We strive to combine advanced medical technology with a caring team of professionals to offer effective treatments and exceptional care.
           </p>
+
           <b className='text-gray-800'> Our Vision</b>
           <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          We envision becoming a leading healthcare institution recognized for our unwavering commitment to patient safety, medical excellence, and innovation. By continuously evolving and adapting to the latest medical advancements, we aim to set new standards in healthcare delivery.
           </p>
+          
         </div>
       </div>
 
@@ -28,13 +30,13 @@ const About = () => {
         <p> WHY <span className='text-gray-700 font-semibold'>CHOOSE US</span></p>
       </div>
 
-      <div className='flex flex-col md:flex-row mb-20'>
+      <div className='flex flex-col md:flex-row mb-20'> 
        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary text-white transition-all duration-300 text-gray cursor-pointer'>
        <b>Efficiency:</b>
        <p> jnwqvnefnjfjknfjnwqvnefnjfjknf </p>
        </div>
 
-       <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary text-white transition-all duration-300 text-gray cursor-pointer'>
+      <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary text-white transition-all duration-300 text-gray cursor-pointer'>
        <b>Convenience:</b>
        <p> jnwqvnefnjfjknf jnwqvnefnjfjknf</p>
        </div>
@@ -42,7 +44,7 @@ const About = () => {
        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary text-white transition-all duration-300 text-gray cursor-pointer'>
        <b>Personalization:</b>
        <p> jnwqvnefnjfjknfjnwqvnefnjfjknf </p>
-       </div>
+       </div> 
 
       </div>
     </div>
